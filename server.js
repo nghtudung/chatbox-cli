a020f4eeddcb5c44691d5643d663d2b371745653
nghtudung <nghtudung@gmail.com>
2025-05-24 23:18:24 +0700

    init

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const PORT = process.env.PORT || 4953;
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('🔌 Một user đã kết nối');
+
+  socket.on('join', (username) => {
+    socket.username = username;
+    console.log(`[LOG] Người dùng mới: ${username}`);
+    socket.broadcast.emit('user-joined', `${username} đã vào phòng chat`);
+  });
+
+  socket.on('chat-message', (msg) => {
+    // Gửi cho tất cả, kể cả người gửi
+    io.emit('chat-message', { user: socket.username, message: msg });
+  });
+
+
+  socket.on('disconnect', () => {
+    if (socket.username)
+      io.emit('user-left', `${socket.username} đã rời phòng chat`);
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`🔥 Server chạy tại http://localhost:${PORT}`);
+});
